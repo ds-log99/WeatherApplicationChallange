edb32ed8115d7a85ac606e2d7bba499cf0275fb6
admin <danielpuscasjr99@gmail.com>
2023-09-11 21:39:07 +0100

    Unit tests improved using Moq framework

--- WeatherApplicationTests/ControllerTests/HomeControllerTests.cs
+++ WeatherApplicationTests/ControllerTests/HomeControllerTests.cs
@@ -3,37 +3,37 @@ using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 
 using Microsoft.Extensions.Logging;
+using Moq;
 using System.Net.WebSockets;
 using WeatherApplication.Controllers;
 using WeatherApplication.Models;
 using WeatherApplication.Repositories;
+
 
 namespace WeatherApplicationTests.ControllerTests
 {
     public class HomeControllerTests
     {
         
-        private ILogger<HomeController> _logger;
-        private IWeatherApiGetCalls _weatherApiGetCalls;
+        private Mock<ILogger<HomeController>> _logger = new Mock<ILogger<HomeController>>();
+        private Mock<IWeatherApiGetCalls> _weatherApiGetCalls = new Mock<IWeatherApiGetCalls>();
+
         private HomeController _homeController;
         public HomeControllerTests()
         {
-            //Dependencies 
-            _logger = A.Fake<ILogger<HomeController>>();
-            _weatherApiGetCalls = A.Fake<IWeatherApiGetCalls>();
-
             //SUT
-            _homeController = new HomeController(_logger, _weatherApiGetCalls);
+            _homeController = new HomeController(_logger.Object, _weatherApiGetCalls.Object);
         }
 
         [Fact]
         public void GetIndexViewSuccess()
         {
             //Arrange
-            var weatherTestModel = A.Fake<WeatherDataModel>();
+         
+            var weatherTestModel = new Mock<WeatherDataModel>();
             string errorMessage = string.Empty;
             //Act 
-            var result = _homeController.Index(weatherTestModel, errorMessage);
+            var result = _homeController.Index(weatherTestModel.Object, errorMessage);
             //Assert 
             result.Should().BeOfType<ViewResult>();
 
@@ -42,28 +42,34 @@ namespace WeatherApplicationTests.ControllerTests
         [Fact]
         public void GetIndexViewWithViewBagSuccess()
         {
+            //Arrange
             string errorMessage = "input valid location test";
 
+            //Act
+            _homeController.Index(null, errorMessage);
+
+            //Assert
             ((string)_homeController.ViewBag.ErrorMessage).Should().Be(errorMessage);
 
         }
-
+        
         [Fact]
-        public void HomeControllerGetWeatherDataSuccess()
+        public async void HomeControllerGetWeatherDataSuccess()
         {
             //Arrange
-            var weatherTestModel = A.Fake<WeatherDataModel>();
+            var weatherTestModel = new Mock<WeatherDataModel>();
             string location = "location";
-            var weatherFakeDateResult =  A.CallTo(() => _weatherApiGetCalls.GetWeatherForecastAsync(location)).Returns(weatherTestModel);
+
+           _weatherApiGetCalls.Setup(x => x.GetWeatherForecastAsync(location)).ReturnsAsync(weatherTestModel.Object);
 
             //Act
-            var result = _homeController.GetWeatherData(weatherTestModel);
+            var result = _homeController.GetWeatherData(weatherTestModel.Object);
 
             //Assert
             result.Should().BeOfType<Task<IActionResult>>();
 
         }
-
+        
 
     }
 }
--- WeatherApplicationTests/GetApiCallsTests/GetWeatherDataTests.cs
+++ WeatherApplicationTests/GetApiCallsTests/GetWeatherDataTests.cs
@@ -3,6 +3,7 @@ using FakeItEasy;
 using FluentAssertions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,17 +17,16 @@ namespace WeatherApplicationTests.GetApiCallsTests
 {
     public class GetWeatherDataTests
     {
-        private ILogger<GetApiCalls> _logger;
-        private IConfiguration _configuration;
+        private Mock<ILogger<GetApiCalls>> _logger = new Mock<ILogger<GetApiCalls>>();
+        private Mock<IWeatherApiGetCalls> _weatherApiGetCalls = new Mock<IWeatherApiGetCalls>();
+
+        // private ILogger<GetApiCalls> _logger;
+        private Mock<IConfiguration> _configuration = new Mock<IConfiguration>();
         private IWeatherApiGetCalls _weatherApiGet;
 
         public GetWeatherDataTests()
         {
-            //Dependencies 
-            _logger = A.Fake<ILogger<GetApiCalls>>();
-            _configuration = A.Fake<IConfiguration>();
-            
-            _weatherApiGet = new GetApiCalls(_logger, _configuration);
+            _weatherApiGet = new GetApiCalls(_logger.Object, _configuration.Object);
         }
 
         [Fact]
@@ -34,26 +34,30 @@ namespace WeatherApplicationTests.GetApiCallsTests
         {
             //Arrange
             string location = "location";
+            var weatherTestModel = new Mock<WeatherDataModel>();
+
+            _weatherApiGetCalls.Setup(x => x.GetWeatherForecastAsync(location)).ReturnsAsync(weatherTestModel.Object);
             //Act
+
             var result = _weatherApiGet.GetWeatherForecastAsync(location);
-                
+
             //Assert
             result.Should().NotBeNull();
         }
 
         [Fact]
-        public void GetWeatherForecastFail()
+        public async void GetWeatherForecastFailLocationInvalid()
         {
             //Assert
-            string location = "ASDADSADASSAD";
-            var weatherTestModel = A.Fake<WeatherDataModel>();
+            string location = "ddddddddd123";
+            var weatherTestModel = new Mock<WeatherDataModel>();
+            _weatherApiGetCalls.Setup(x => x.GetWeatherForecastAsync(location));
 
             //Act
-
-            var result = _weatherApiGet.GetWeatherForecastAsync(location);
+            var result = await _weatherApiGet.GetWeatherForecastAsync(location);
 
             //Assert
-            result.Equals(weatherTestModel);
+            result.Should().BeNull();
 
         }
 
